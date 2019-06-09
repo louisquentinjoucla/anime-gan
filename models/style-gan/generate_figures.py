75d1a42ce06519c846f59b638631dcd189e3abae
louisquentinjoucla <louis-quentin.joucla@isen.yncrea.fr>
2019-06-08 22:41:13 -0400

    gen v5

--- models/style-gan/generate_figures.py
+++ models/style-gan/generate_figures.py
@@ -23,7 +23,7 @@ url_celebahq    = 'https://drive.google.com/uc?id=1MGqJl28pN4t7SAtSrPdSRJSQJqahk
 url_bedrooms    = 'https://drive.google.com/uc?id=1MOSKeGF0FJcivpBI7s63V9YHloUTORiF' # karras2019stylegan-bedrooms-256x256.pkl
 url_cars        = 'https://drive.google.com/uc?id=1MJ6iCfNtMIRicihwRorsM3b7mmtmK9c3' # karras2019stylegan-cars-512x384.pkl
 url_cats        = 'https://drive.google.com/uc?id=1MQywl0FNt6lHu8E_EUqnRbviagS7fbiJ' # karras2019stylegan-cats-256x256.pkl
-url_anime       = '/home/louisquentinjoucla/anime-gan/models/style-gan/results/00000-sgan-recordanime-4gpu/network-snapshot-004885.pkl'
+url_anime       = 'network-snapshot-004885.pkl'
 
 synthesis_kwargs = dict(output_transform=dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True), minibatch_size=8)
 
@@ -38,8 +38,9 @@ def load_Gs(url):
 
 def load_Gs2(url):
     if url not in _Gs_cache:
-        _G, _D, Gs = pickle.load(url)
-        _Gs_cache[url] = Gs
+        with open(url, 'rb') as f:
+            _G, _D, Gs = pickle.load(f)
+            _Gs_cache[url] = Gs
     return _Gs_cache[url]
 
 #----------------------------------------------------------------------------
